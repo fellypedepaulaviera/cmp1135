c2d239c650426d03be26963d801cce5cec77ef5f
heider23 <heider23@hotmail.com>
2017-04-10 23:09:13 -0300

    Testa a classe Numero.

---
+++ src/exemplos/TesteNumero.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package exemplos;
+
+/**
+ * @author Heider
+ *
+ */
+public class TesteNumero {
+
+	public static void main(String[] args) {
+		Numero n1 = new Numero();
+		n1.setValor(10);
+		Numero n2 = new Numero();
+		n2.setValor(20);
+		System.out.println(n1.getValor() + ":" + n2.getValor());
+	}
+	
+}
