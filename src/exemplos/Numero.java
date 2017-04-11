b837a8092deb913f45c48c1647214ed9c758694d
heider23 <heider23@hotmail.com>
2017-04-10 23:11:34 -0300

    Classe para exemplificar o uso de variaveis estaticas.

---
+++ src/exemplos/Numero.java
@@ -0,0 +1,15 @@
+package exemplos;
+
+public class Numero {
+	
+	static int valor;
+	
+	public int getValor() {
+		return Numero.valor;
+	}
+	
+	public void setValor(int valor) {
+		Numero.valor = valor;
+	}
+
+}
